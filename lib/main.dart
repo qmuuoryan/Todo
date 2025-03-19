6bab4b68c6e38b314dd7b7c1e73c24948c0b1461
muuo <muuo@localhost.localdomain>
2025-03-19 11:16:13 +0300

    creating my first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runapp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+ 
+
+
